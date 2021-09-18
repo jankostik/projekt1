44618616fdd740c4d241b246a87ecf9cb53e922c
Jan Kostohryz <jan.kostik5@gmail.com>
2021-09-18 14:56:38 +0200

    pridal jsem zakladni funkce pro ziskavani dat z datavaze

---
+++ temp/cache/nette.robotLoader/33bce9f260021a660da1949b8c0b7cb4.php
@@ -0,0 +1,80 @@
+<?php
+return array (
+  0 => 
+  array (
+    'App\\Model\\DatabaseManager' => 
+    array (
+      0 => '/var/www/sites/projekt1/app/Model/DatabaseManager.php',
+      1 => 1631723793,
+    ),
+    'App\\Model\\UserFacade' => 
+    array (
+      0 => '/var/www/sites/projekt1/app/Model/UserFacade.php',
+      1 => 1631089707,
+    ),
+    'App\\Model\\DuplicateNameException' => 
+    array (
+      0 => '/var/www/sites/projekt1/app/Model/UserFacade.php',
+      1 => 1631089707,
+    ),
+    'App\\Bootstrap' => 
+    array (
+      0 => '/var/www/sites/projekt1/app/Bootstrap.php',
+      1 => 1631089707,
+    ),
+    'App\\Forms\\FormFactory' => 
+    array (
+      0 => '/var/www/sites/projekt1/app/Forms/FormFactory.php',
+      1 => 1631089707,
+    ),
+    'App\\Forms\\SignUpFormFactory' => 
+    array (
+      0 => '/var/www/sites/projekt1/app/Forms/SignUpFormFactory.php',
+      1 => 1631089707,
+    ),
+    'App\\Forms\\SignInFormFactory' => 
+    array (
+      0 => '/var/www/sites/projekt1/app/Forms/SignInFormFactory.php',
+      1 => 1631089707,
+    ),
+    'App\\Router\\RouterFactory' => 
+    array (
+      0 => '/var/www/sites/projekt1/app/Router/RouterFactory.php',
+      1 => 1631089707,
+    ),
+    'App\\Presenters\\ErrorPresenter' => 
+    array (
+      0 => '/var/www/sites/projekt1/app/Presenters/ErrorPresenter.php',
+      1 => 1631089707,
+    ),
+    'App\\Presenters\\Error4xxPresenter' => 
+    array (
+      0 => '/var/www/sites/projekt1/app/Presenters/Error4xxPresenter.php',
+      1 => 1631089707,
+    ),
+    'App\\Presenters\\SignPresenter' => 
+    array (
+      0 => '/var/www/sites/projekt1/app/Presenters/SignPresenter.php',
+      1 => 1631089707,
+    ),
+    'App\\Presenters\\BasePresenter' => 
+    array (
+      0 => '/var/www/sites/projekt1/app/Presenters/BasePresenter.php',
+      1 => 1631089707,
+    ),
+    'App\\Presenters\\HomepagePresenter' => 
+    array (
+      0 => '/var/www/sites/projekt1/app/Presenters/HomepagePresenter.php',
+      1 => 1631089707,
+    ),
+  ),
+  1 => 
+  array (
+    'App\\Presenters\\HomepageDefaultTemplate' => 1,
+    'App\\Presenters\\HomepageTemplate' => 1,
+  ),
+  2 => 
+  array (
+    '/var/www/sites/projekt1/app/SitesModule/Module/GamesManager.php' => 1631820429,
+  ),
+);
